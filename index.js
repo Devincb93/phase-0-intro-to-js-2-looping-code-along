824b790307bc5d00cf3a60402de9b1af3fcbe3a1
Devin <devincb93@gmail.com>
2023-06-18 03:24:40 -0700

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const messages = [];
+
+    for (let i = 0; i < cards.length; i++) {
+       messages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+
+    }
+    return messages;
+}
+writeCards(cards);
+
+function countDown(number) {
+    for (let i = 10; i >= 0; i--)
+    console.log(i);
+}
+countDown(10);
